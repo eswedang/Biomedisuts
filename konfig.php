4978f06b7c184fd25f94d4576cde6f01ac98eab1
eswedang <firzanaufal@std.unissula.ac.id>
2023-11-06 20:17:46 +0700

    first commit

---
+++ konfig.php
@@ -0,0 +1,17 @@
+<?php
+/*
+|--------------------------------------------------------------------------
+| Aksi login
+|--------------------------------------------------------------------------
+|
+|   Aplikasi Sistem Informasi Rumah Sakit Sederhana
+|   http://www.sarjanakomedi.com
+|
+*/
+// error_reporting(0);
+
+        
+    $base_url = "http://localhost:2000/sirm/";
+	
+	$con = mysqli_connect('localhost', 'root', '', 'sirm');
+?>
